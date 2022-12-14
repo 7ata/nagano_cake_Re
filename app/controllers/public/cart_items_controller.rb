83b05702efe78f75f2c9bf45eb96f7e59a5c4893
7ata <103552711+7ata@users.noreply.github.com>
2022-12-14 09:02:54 +0000

    [add]カート画面create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,12 +7,28 @@ class Public::CartItemsController < ApplicationController
 
   def create
 
-    @cart_item = CartItem.new(cart_item_params)
+    #@cart_item = CartItem.new(cart_item_params)
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+        # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
     @cart_item.customer_id = current_customer.id
 
-    if @cart_item.save
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      #元々カート内にあるもの「item_id」
+                          #今追加した　　　　　　　params[:cart_item][:item_id])
+
+       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+            cart_item.amount += params[:cart_item][:amount].to_i
+            #cart_item.amountに今追加したparams[:cart_item][:amount]を加える
+                                                              #.to_iとして数字として扱う
+
+       cart_item.save
+            redirect_to cart_items_path, notice: "商品の数を追加しました"
+
+    # もしカート内に「同じ」商品がない場合は通常の保存処理
+    elsif @cart_item.save
   		redirect_to cart_items_path, notice: "商品をカートに入れました"
-    else
+
+    else# 保存できなかった場合
       @cart_items = CartItem.all
       render :index
     end
@@ -43,6 +59,6 @@ class Public::CartItemsController < ApplicationController
 
   private
   def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+      params.require(:cart_item).permit(:item_id, :amount,:customer_id)
   end
 end
