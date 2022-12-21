70f44e9decd9ddf0f76a0586d4400c4cf01946c6
7ata <103552711+7ata@users.noreply.github.com>
2022-12-21 13:29:48 +0000

    [Update] 注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,13 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @customer = current_customer
+    @addresses = current_customer.addresses
   end
 
   def confirm
+    @customer = current_customer
+    #ここに注文情報入力の部分　条件分岐　まだcreateじゃない
   end
 
   def complete
