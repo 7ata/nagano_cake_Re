3dda3a9c274d95e838f17b6a66fe913b9b01f9d2
7ata <103552711+7ata@users.noreply.github.com>
2023-01-02 15:46:22 +0000

    [Update] 注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,29 +2,36 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @customer = current_customer
-    @addresses = current_customer.addresses
+    @addresses = current_customer.addresses.all
   end
 
   def confirm
     @order = Order.new(order_params)
-    #ここに注文情報入力の部分　条件分岐　まだcreateじゃない
-    @cart_items = current_end_user.cart_items
-		@order.payment = params[:order][:payment]
-		if params[:order][:address_option] == "0"#ご自身の住所選択時
+    #ここに注文情報入力の部分 条件分岐 まだcreateじゃない
+    @cart_items = current_customer.cart_items.all
+    @total = 0
+		@order.payment_method = params[:order][:payment_method]
+
+    if params[:order][:address_option] == "0"#ご自身の住所選択時
 		  @order.postal_code = current_customer.postal_code
 			@order.address = current_customer.address
 			@order.name = current_customer.fast_name + current_customer.last_name
+
 		elsif params[:order][:address_option] == "1"#登録済み住所から選択
-		  @address = Address.find(params[:order][:address_id])
-		  #orderの_id(=カラム)でアドレス(帳)を選び、そのデータ送る
-      @order.postal_code = @address.post_code
+		  @address = Address.find(params[:order][:registered])
+		  #orderのaddress_id(=カラム)でアドレス(帳)を選び、そのデータ送る
+		  #params[:order][:address_option] == "0"から"2"の 住所データを定義する
+      @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif
 
-
-
-
+    elsif params[:order][:address_option] = "2"#新しいお届け先選択
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    else
+          render 'new'
+    end
 
 
   end
@@ -33,6 +40,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
   end
 
   def index
