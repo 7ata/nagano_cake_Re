f81960ba62083d7eb8c4001cad73e853e3ba6a85
7ata <103552711+7ata@users.noreply.github.com>
2023-01-03 16:12:08 +0000

    [Update]注文情報確認画面

--- db/schema.rb
+++ db/schema.rb
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 2022_11_09_105211) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "name", null: false
-    t.integer "shopping_cost", null: false
+    t.integer "shipping_cost", null: false
     t.integer "total_payment", null: false
     t.integer "payment_method", null: false
     t.integer "status", null: false
