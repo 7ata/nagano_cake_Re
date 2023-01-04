fa97a859d0e0f6e57680bdf299e5cdc1bc62594e
7ata <103552711+7ata@users.noreply.github.com>
2023-01-04 09:04:42 +0000

    [Update]注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,26 +12,26 @@ class Public::OrdersController < ApplicationController
     @total = 0
 		@order.payment_method = params[:order][:payment_method]
 
-    if params[:order][:address_option] == "0"#ご自身の住所選択時
+		if params[:order][:address_option] == "0"#ご自身の住所選択時
 		  @order.postal_code = current_customer.postal_code
 			@order.address = current_customer.address
 			@order.name = current_customer.fast_name + current_customer.last_name
 
 		elsif params[:order][:address_option] == "1"#登録済み住所から選択
-		  @address = Address.find(params[:order][:registered])
-		  #orderのaddress_id(=カラム)でアドレス(帳)を選び、そのデータ送る
+		  @address = Address.find(params[:order][:customer_id])
+		  #orderのmember_id(=カラム)でアドレス(帳)を選び、そのデータ送る
 		  #params[:order][:address_option] == "0"から"2"の 住所データを定義する
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
-
-    elsif params[:order][:address_option] = "2"#新しいお届け先選択
+    # 新規住所入力 [:address_option]=="2"としてデータをhtmlから受ける
+		elsif params[:order][:address_option] = "2"#新しいお届け先選択
       @order.postal_code = params[:order][:postal_code]
       @order.address = params[:order][:address]
       @order.name = params[:order][:name]
-    else
+		else
           render 'new'
-    end
+		end
 
 
   end
